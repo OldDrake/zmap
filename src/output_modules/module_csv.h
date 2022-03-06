a30a9e6128c70a0a02b117e08d82172f6efe07a5
OldDrake <33509198+OldDrake@users.noreply.github.com>
2022-03-06 23:26:42 +0800

    new csv4rdns module added

--- src/output_modules/module_csv.h
+++ src/output_modules/module_csv.h
@@ -12,3 +12,8 @@
 int csv_init(struct state_conf *conf, char **fields, int fieldlens);
 int csv_process(fieldset_t *fs);
 int csv_close(struct state_conf *c, struct state_send *s, struct state_recv *r);
+
+// output module for rdns scan
+int csv4rdns_init(struct state_conf *conf, char **fields, int fieldlens);
+int csv4rdns_process(fieldset_t *fs);
+int csv4rdns_close(struct state_conf *c, struct state_send *s, struct state_recv *r);
